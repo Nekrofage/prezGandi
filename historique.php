e5a013829d5e986b0fc7fd1934fcb8d27b2ba68a
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2019-12-04 22:13:36 +0100

    Add pages

--- historique.php
+++ historique.php
@@ -4,6 +4,7 @@
                     <br> 
                     Fondée en mars 2000<br> 
                     Racheté en septembre 2005<br> 
+                    Fait parti d'un fond d'investissement<br>
                     <br/>
                     <br/>
                     <p>
