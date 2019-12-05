aa39a1bc0138c7b5eba3b03785fbcd012ace462e
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2019-12-05 19:43:34 +0100

    Add pages

--- description2.php
+++ description2.php
@@ -2,8 +2,8 @@
                     <h3>Gandi</h3>
                     <br>
                     Langues des clients : France, Anglais, Espagnol, Allemand, Chinois, Japonais<br>
-                    Clients : Particulier, Revendeur (8000), corporate (800)<br>
-                    Service Client / Customer Care : 24h/24 - 6 jours/7<br> 
+                    Clients : Particulier, Revendeur (8000), Corporate (800)<br>
+                    Customer Care : 24h/24 - 6 jours/7<br> 
                     2,5 Millions de noms de domaine<br/>
                     140 collaborateurs<br>
                     200 000 sites/app hébergé<br>
--- role.php
+++ role.php
@@ -3,9 +3,9 @@
                     <h4>Son rôle</h4>
                     <br> 
                     Etre à l'écoute du client<br>
-                    Tampon entre le client et l'équipe technique (dev, ops, etc.)<br>
+                    Contact entre le client et l'équipe technique Gandi<br>
                     Connaître les produits Gandi<br>
-                    Conseiller le client<br>
+                    Conseiller le client aux mieux<br>
                     Etre réactif et pro-actif<br>
                     Pas de vente / commerciale<br>
                     <br> 
